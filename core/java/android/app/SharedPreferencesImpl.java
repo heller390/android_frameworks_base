36bebc1b4f19e6a5d579b8189f076b34c78c7ab6
GrapheneOS <contact@grapheneos.org>
2023-06-06 14:44:05 +0300

    gmscompat: add compatibility layer for unprivileged GMS
    Originally authored by Danny Lin <danny@kdrag0n.dev> for inclusion in
    GrapheneOS. It has since been substantially extended and partially
    rewritten by Dmitry Muhomor <muhomor.dmitry@gmail.com> (primarily) and
    Daniel Micay <danielmicay@gmail.com>.
    
    Commit history:
    (better viewed with git log --grep gmscompat $TAG_NAME)
    
    February 2023 - May 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ2A.230505.002.2023052900
    
    August 2022 - February 2023:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ1A.230205.002.2023020600
    
    June 2022 - August 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.006.2022073000
    
    October 2021 - June 2022:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.002.2022053100
    
    July 2021 - October 2021:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/RQ3A.211001.001.2021100606
    
    Change-Id: I26a1f56c9796e19bb7d4c1b5702f376b5f65f9c2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/SharedPreferencesImpl.java
+++ core/java/android/app/SharedPreferencesImpl.java
@@ -17,7 +17,11 @@
 package android.app;
 
 import android.annotation.Nullable;
+
 import android.baikalos.AppProfile;
+
+import android.app.compat.gms.GmsCompat;
+
 import android.compat.Compatibility;
 import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
@@ -33,6 +37,7 @@ import android.system.StructTimespec;
 import android.util.Log;
 
 import com.android.internal.annotations.GuardedBy;
+import com.android.internal.gmscompat.GmsHooks;
 import com.android.internal.util.ExponentiallyBucketedHistogram;
 import com.android.internal.util.XmlUtils;
 
@@ -290,11 +295,24 @@ final class SharedPreferencesImpl implements SharedPreferences {
 
     @Override
     public Map<String, ?> getAll() {
+        HashMap<String, Object> res;
         synchronized (mLock) {
             awaitLoadedLocked();
             //noinspection unchecked
-            return new HashMap<String, Object>(mMap);
+            res = new HashMap<String, Object>(mMap);
         }
+
+        if (GmsCompat.isEnabled()) {
+            String fileName = mFile.getName();
+            String suffix = ".xml";
+            if (fileName.endsWith(suffix)) {
+                int endIndex = fileName.length() - suffix.length();
+                String name = fileName.substring(0, endIndex);
+                GmsHooks.maybeModifySharedPreferencesValues(name, res);
+            }
+        }
+
+        return res;
     }
 
     @Override
--- core/java/android/os/Binder.java
+++ core/java/android/os/Binder.java
@@ -20,12 +20,15 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.SystemApi;
 import android.app.AppOpsManager;
+import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.util.ExceptionUtils;
 import android.util.Log;
 import android.util.Slog;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.gmscompat.BinderRedirector;
+import com.android.internal.gmscompat.GmsHooks;
 import com.android.internal.os.BinderCallHeavyHitterWatcher;
 import com.android.internal.os.BinderCallHeavyHitterWatcher.BinderCallHeavyHitterListener;
 import com.android.internal.os.BinderInternal;
@@ -656,7 +659,18 @@ public class Binder implements IBinder {
     public void attachInterface(@Nullable IInterface owner, @Nullable String descriptor) {
         mOwner = owner;
         mDescriptor = descriptor;
+
+        if (BinderRedirector.enabled()) {
+            mPerformRedirectionCheck = "com.google.android.gms.common.internal.IGmsCallbacks".equals(descriptor);
+        }
+
+        if (GmsCompat.isGmsCore()) {
+            mIsGmsServiceBroker = GmsHooks.GMS_SERVICE_BROKER_INTERFACE_DESCRIPTOR.equals(descriptor);
+        }
     }
+
+    private boolean mPerformRedirectionCheck;
+    private boolean mIsGmsServiceBroker;
 
     /**
      * Default implementation returns an empty interface name.
@@ -1215,6 +1229,8 @@ public class Binder implements IBinder {
         sWorkSourceProvider = workSourceProvider;
     }
 
+    private volatile int mPreviousUid;
+
     // Entry point from android_util_Binder.cpp's onTransact.
     @UnsupportedAppUsage
     private boolean execTransact(int code, long dataObj, long replyObj,
@@ -1222,6 +1238,16 @@ public class Binder implements IBinder {
         // At that point, the parcel request headers haven't been parsed so we do not know what
         // {@link WorkSource} the caller has set. Use calling UID as the default.
         final int callingUid = Binder.getCallingUid();
+        if (GmsCompat.isEnabled()) {
+            if (callingUid != mPreviousUid) {
+                // harmless race
+                mPreviousUid = callingUid;
+                if (Process.isApplicationUid(callingUid)) {
+                    GmsHooks.onBinderTransaction(Binder.getCallingPid(), callingUid);
+                }
+            }
+        }
+
         final long origWorkSource = ThreadLocalWorkSource.setUid(callingUid);
         try {
             return execTransactInternal(code, dataObj, replyObj, flags, callingUid);
@@ -1245,6 +1271,7 @@ public class Binder implements IBinder {
         // Log any exceptions as warnings, don't silently suppress them.
         // If the call was {@link IBinder#FLAG_ONEWAY} then these exceptions
         // disappear into the ether.
+
         final boolean tagEnabled = Trace.isTagEnabled(Trace.TRACE_TAG_AIDL);
         final boolean hasFullyQualifiedName = getMaxTransactionId() > 0;
         final String transactionTraceName;
@@ -1267,7 +1294,14 @@ public class Binder implements IBinder {
         }
 
         final boolean tracingEnabled = tagEnabled && transactionTraceName != null;
+
+        data.mPerformBinderRedirectionCheck = mPerformRedirectionCheck;
+        boolean onBeginGmsServiceBrokerCallRet = false;
+
         try {
+            if (mIsGmsServiceBroker) {
+                onBeginGmsServiceBrokerCallRet = GmsHooks.onBeginGmsServiceBrokerCall(code, data);
+            }
             final BinderCallHeavyHitterWatcher heavyHitterWatcher = sHeavyHitterWatcher;
             if (heavyHitterWatcher != null) {
                 // Notify the heavy hitter watcher, if it's enabled.
@@ -1308,6 +1342,10 @@ public class Binder implements IBinder {
             }
             res = true;
         } finally {
+            data.mPerformBinderRedirectionCheck = false;
+            if (onBeginGmsServiceBrokerCallRet) {
+                GmsHooks.onEndGmsServiceBrokerCall();
+            }
             if (tracingEnabled) {
                 Trace.traceEnd(Trace.TRACE_TAG_AIDL);
             }
